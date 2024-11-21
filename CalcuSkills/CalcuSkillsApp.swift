81f35362eda71aeb8f1555aae40135ce36926773
Papayanew <johannes.berrou@gmail.com>
2024-11-21 16:35:16 +0100

    Initial Commit

---
+++ CalcuSkills/CalcuSkillsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcuSkillsApp.swift
+//  CalcuSkills
+//
+//  Created by Johannes on 21.11.24.
+//
+
+import SwiftUI
+
+@main
+struct CalcuSkillsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
